8c6c6a8a198b9df3552684bf7d46b77b1cc24ec1
Matteo Anselmo <matteo.anselmo96@gmail.com>
2022-05-19 17:52:20 +0200

    piccola modifica welcome page

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -89,14 +89,19 @@
                 </div>
 
                 <div class="links">
-                    <a href="https://laravel.com/docs">Docs</a>
+                    {{-- <a href="https://laravel.com/docs">Docs</a>
                     <a href="https://laracasts.com">Laracasts</a>
                     <a href="https://laravel-news.com">News</a>
                     <a href="https://blog.laravel.com">Blog</a>
                     <a href="https://nova.laravel.com">Nova</a>
                     <a href="https://forge.laravel.com">Forge</a>
                     <a href="https://vapor.laravel.com">Vapor</a>
-                    <a href="https://github.com/laravel/laravel">GitHub</a>
+                    <a href="https://github.com/laravel/laravel">GitHub</a> --}}
+                    @if (Auth::check())
+                    <a href="{{route('andim.posts.index')}}">guarda i post</a>
+                        @else
+                        <a href="{{route('register')}}">Registati!</a>
+                    @endif
                 </div>
             </div>
         </div>
